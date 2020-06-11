fe4bb5d7a28124bc49210351f3f7069939fde503
Mandi <mandi018@gmail.com>
2020-06-10 20:28:46 -0500

    Renamed app and main page to something more custom to this particular app

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ import './views/new_shopping_list.dart';
 import './views/new_store.dart';
 import './views/existing_item.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(GroceryGoApp());
 
-class MyApp extends StatelessWidget {
+class GroceryGoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +34,21 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Grocery Go!'),
+      home: MainPage(title: 'Grocery Go!'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainPageState extends State<MainPage> {
 
   // Some placeholder data just so we can see things working
   final List<ShoppingList> shoppingLists = [
